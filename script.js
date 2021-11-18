7d7c316d49a04adc256a651442962cbb4c09ccdd
vivek kumar <57774129+Vivek-jpg@users.noreply.github.com>
2021-11-18 21:27:03 +0000

    fed

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const Input1 = document.querySelector('.input1');
 const Input2 = document.querySelector('.input2');
+const Input3 = document.querySelector('.input3');
+const Output = document.querySelector('.output');
+
+
 const Btn = document.querySelector('.btn');
 const body = document.querySelector('body');
 
