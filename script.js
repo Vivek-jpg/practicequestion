3155dd5c45d709c37bfb6e8eeaf2ed0fdc4fd77e
vivek kumar <57774129+Vivek-jpg@users.noreply.github.com>
2021-11-18 21:25:15 +0000

    fed

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
 const Input1 = document.querySelector('.input1');
 const Input2 = document.querySelector('.input2');
+const Btn = document.querySelector('.btn');
 const body = document.querySelector('body');
 
 
 function Switchcolor(){
   console.log("click");
-  var Inputname = Input1.value;
-  var Inputcolor = Input2.value;
-  if(Inputname > Inputcolor){
-    Input1.style.backgroundColor = "green";
-    Input2.style.backgroundColor = 'red';
-    body.style.background ="blue";
-  }
-  else
-  if(Inputcolor > Inputname){
-     Input2.style.backgroundColor = 'green';
-     Input1.style.backgroundColor = 'red';
-      body.style.background ="gray";
-      
-
-     
-  }
+  
 }
-setInterval(Switchcolor,3000)
 
 
-Input2.addEventListener("input", Switchcolor);
+
+Btn.addEventListener("click", Switchcolor);
